673d2f298434703d7ba1a44d694b294c8b0b56d2
yishai <yishai5555@gmail.com>
2023-10-30 19:06:29 +0200

    05-search-google

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import './App.css';
 function App() {
   const [userQuery, setUserQuery] = useState('');
 
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  };
+
   const updateUserQuery = (event) => {
     console.log({ userQuery });
 
@@ -16,7 +20,7 @@ function App() {
       <h1>React Hooks</h1>
       <div className='form'>
         <input value={userQuery} onChange={updateUserQuery} />
-        <button>Search</button>
+        <button onClick={searchQuery}>Search</button>
       </div>
     </div>
   );

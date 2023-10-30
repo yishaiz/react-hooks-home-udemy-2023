98ca59775887db4155e09f5326eba37a897e7660
yishai <yishai5555@gmail.com>
2023-10-30 19:02:50 +0200

    04

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [userQuery, setUserQuery] = useState('');
+
+  const updateUserQuery = (event) => {
+    console.log({ userQuery });
+
+    setUserQuery(event.target.value);
+  };
 
   return (
-    <>
-      <div className='App'>React Hooks</div>
-    </>
+    <div className='App'>
+      <h1>React Hooks</h1>
+      <div className='form'>
+        <input value={userQuery} onChange={updateUserQuery} />
+        <button>Search</button>
+      </div>
+    </div>
   );
 }
 
